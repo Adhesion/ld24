dc802cfb71ce3ecc2968da24d0914245f1a1fd56
Andrew Ford <adhesionmusic@gmail.com>
2012-08-26 22:36:55 -0400

    add bomb, move spawnparticle to global

--- gameResources.js
+++ gameResources.js
@@ -27,6 +27,7 @@ var gameResources =
     { name: "explode", type: "image", src: "data/explode.png" },
 
     { name: "rock", type: "image", src: "data/rocktemp.png" },
+    { name: "bomb", type: "image", src: "data/bomb.png" },
     { name: "spikes", type: "image", src: "data/spike.png" },
     { name: "balloon", type: "image", src: "data/baloon.png" },
 
